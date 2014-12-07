cbf3c3f46c5c8e1b889842bd62840a2b4c0080d3
longtaoge <61852263@qq.com>
2014-12-07 20:15:59 +0800

    forEclipse
    转为Eclipse 工程

---
+++ ForEclipse/MainActivity1/gen/com/github/ksoichiro/android/observablescrollview/samples/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.github.ksoichiro.android.observablescrollview.samples;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
